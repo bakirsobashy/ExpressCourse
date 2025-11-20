ef85de39914e9fe6556b50114ccb85852d51ee51
bakirsobashy <bakir1998bt.bk@gmail.com>
2025-11-20 18:49:54 +0000

    not working

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = 3000
 
-const arr = [1,2,3,4,5]
+app.use(express.json())
+app.use(cookieParser())
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
 
 app.get("/", (req, res) => {
-  const {name , id} = req.query
-  console.log(arr[parseInt(id)])
-  res.send(req.query)
+  res.send("Hello World!")
+})
+
+app.get("/sign", (req, res) => {
+  const username = 'user'
+  const password = 'pass'
+  res.cookie(username,password).send("Cookie has been set")
+})
+
+app.get("/login", (req, res) => {
+  const cookies = req.cookie
+  if (cookies) {
+    res.send("logged in")
+    console.log(cookies)
+  } else {
+    res.send("No cookies found")
+  }
 })
