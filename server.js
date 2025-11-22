d7b829b266f275182e471b0f2a2aa4edbcbb1826
bakirsobashy <bakir1998bt.bk@gmail.com>
2025-11-22 18:03:50 +0300

    fixed server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
