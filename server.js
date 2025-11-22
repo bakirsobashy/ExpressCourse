0b950fba5172f66bf5b9d1f19cb7fa4d8bda3824
bakirsobashy <bakir1998bt.bk@gmail.com>
2025-11-22 14:47:36 +0300

    hello world server

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
-const express = require("express")
-const cookieParser = require('cookie-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-
-
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
-
-app.get("/sign", (req, res) => {
-  const username = 'user'
-  const password = 'pass'
-  res.cookie(username,password).send("Cookie has been set")
-})
-
-app.get("/login", (req, res) => {
-  const cookies = req.cookie
-  if (cookies) {
-    res.send("logged in")
-    console.log(cookies)
-  } else {
-    res.send("No cookies found")
-  }
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
